2c2d46a7ee728e5e2c84eb12b2fc125fef2229de
OKhorshid <okhorshid.00@gmail.com>
2024-06-22 02:44:43 +0300

    mo7awla

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,38 @@
 import Image from "next/image";
-import { FormEvent } from 'react'
+import { FormEvent, useState } from 'react'
 
 export default function Home() {
-  
-  async function onSubmit(event: HTMLFormElement) {
-    event.preventDefault()
-    const formData = new FormData(event.currentTarget)
-    const response = await fetch('/api/submit', {
+const [name, setName] = useState('');
+  const [file, setFile] = useState<File | null>(null);
+
+  const handleSubmit = async (event: FormEvent) => {
+   "use server"
+    event.preventDefault();
+    if (!file) return; // Ensure there is a file selected
+    
+    const formData = new FormData();
+    formData.append('name', name);
+    formData.append('file', file!);
+
+    // POST the formData to your API endpoint
+    const response = await fetch('/api/upload', {
       method: 'POST',
       body: formData,
-    })
- 
-    // Handle response if necessary
-    const data = await response.json()
-    // ...
+    });
 
-  }    
+    const result = await response.json();
+    console.log(result); // Process the response here
+  };
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <section className="">
-        <form action="">
-          <input type='text'/>
+        <form onSubmit={handleSubmit}>
+          <input type='text' value={name} onChange={e => setName(e.target.value)} />
           <br/>
-          <input type="file"/>
-          <button className="border border-white">submit</button>
+          <input type="file" onChange={e => setFile(e.target.files ? e.target.files[0] : null)} 
+        required/>
+          <button type="submit" className="border border-white">submit</button>
         </form>
       </section>  
     </main>
