d1e12aceb0aa99d1b37b8ae0424e5e2a5510c0a4
OKhorshid <okhorshid.00@gmail.com>
2024-06-23 20:21:40 +0300

    hotfix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,11 @@
-import { Form } from "./components/form";
+import { Form } from "../components/form";
 
 export default function Home() {
-
-
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <section className="">
-        <Form/>
-      </section>  
+        <Form />
+      </section>
     </main>
   );
 }
