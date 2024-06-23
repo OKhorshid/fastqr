67aa3d37f0c3d588c41a49d13dbbe89976e1a0e1
OKhorshid <okhorshid.00@gmail.com>
2024-06-23 20:03:19 +0300

    server uploads with vercel blob

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,12 @@
-import Image from "next/image";
-import { FormEvent } from 'react'
-import { put } from "@vercel/blob";
-
+import { Form } from "./components/form";
 
 export default function Home() {
 
-  const handleSubmit = async (event: FormEvent) => {
-   alert("submitted, file not recieved by server throw...")
-  };
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <section className="">
-        <form onSubmit={handleSubmit}>
-          <input type='text' name="link" value='' />
-          <br/>
-          <input type="file" name="menu" required/>
-          <button type="submit" className="border border-white">submit</button>
-        </form>
+        <Form/>
       </section>  
     </main>
   );
