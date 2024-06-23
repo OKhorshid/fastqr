0bec1ed2ec163926b95fdb8aa07fa8abda66b748
OKhorshid <okhorshid.00@gmail.com>
2024-06-23 19:21:07 +0300

    installin vercel & recieving the file on client

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,23 @@
 import Image from "next/image";
 import { FormEvent, useState } from 'react'
+import { put } from "@vercel/blob";
+
 
 export default function Home() {
 const [name, setName] = useState('');
   const [file, setFile] = useState<File | null>(null);
 
   const handleSubmit = async (event: FormEvent) => {
-   "use server"
-    event.preventDefault();
-    if (!file) return; // Ensure there is a file selected
-    
-    const formData = new FormData();
-    formData.append('name', name);
-    formData.append('file', file!);
-
-    // POST the formData to your API endpoint
-    const response = await fetch('/api/upload', {
-      method: 'POST',
-      body: formData,
-    });
-
-    const result = await response.json();
-    console.log(result); // Process the response here
+   alert("submitted, file not recieved by server throw...")
   };
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <section className="">
         <form onSubmit={handleSubmit}>
-          <input type='text' value={name} onChange={e => setName(e.target.value)} />
+          <input type='text' name="link" value='' />
           <br/>
-          <input type="file" onChange={e => setFile(e.target.files ? e.target.files[0] : null)} 
-        required/>
+          <input type="file" name="menu" required/>
           <button type="submit" className="border border-white">submit</button>
         </form>
       </section>  
