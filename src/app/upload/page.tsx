1b7a73cef6a3619247b5c220b61665e3444863b5
OKhorshid <okhorshid.00@gmail.com>
2024-06-25 17:28:50 +0300

    added better UI & UX

--- src/app/upload/page.tsx
+++ src/app/upload/page.tsx
@@ -26,25 +26,40 @@ export default function AvatarUploadPage() {
       });
 
       if (!response.ok) {
+        setMessage("Failed to upload");
         throw new Error("Failed to upload");
       }
 
       const newBlob = (await response.json()) as PutBlobResult;
       setBlob(newBlob);
     } catch (error) {
-      setMessage("upload successful");
+      setMessage("Failed to upload");
     } finally {
       setIsLoading(false); // Re-enable the button by setting isLoading to false
+      setMessage("upload successful");
     }
   };
   return (
     <>
-      <h1>Upload Your Avatar</h1>
+      <h1>Upload Your File</h1>
 
       <form onSubmit={handleSubmit}>
         <input name="file" ref={inputFileRef} type="file" required />
-        <button type="submit">Upload</button>
+        <button
+          type="submit"
+          className={`ml-5 border-2 p-2 ${
+            isLoading
+              ? "bg-purple-950 border-slate-500"
+              : "bg-blue-800 border-white"
+          }`}
+          disabled={isLoading}
+        >
+          Upload
+        </button>
       </form>
+      {message && (
+        <div className="my-3 text-green-400 font-bold">{message}</div>
+      )}
       {blob && (
         <div>
           Blob url: <a href={blob.url}>{blob.url}</a>
