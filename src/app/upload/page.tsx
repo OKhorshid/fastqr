c6d4e98b03dc5d97114475cc643a232d973db52a
OKhorshid <okhorshid.00@gmail.com>
2024-06-30 14:19:12 +0300

    use async/await instead of promises

--- src/app/upload/page.tsx
+++ src/app/upload/page.tsx
@@ -10,7 +10,7 @@ export default function AvatarUploadPage() {
   const [isLoading, setIsLoading] = useState(false);
   const [message, setMessage] = useState("");
 
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
     if (!inputFileRef.current?.files) {
@@ -21,58 +21,26 @@ export default function AvatarUploadPage() {
     setIsLoading(true); // Disable the button by setting isLoading to true
     const file = inputFileRef.current.files[0];
 
-    fetch(`/api/upload?filename=${file.name}`, {
-      method: "POST",
-      body: file,
-    })
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("Failed to upload");
-        }
-        return response.json();
-      })
-      .then((newBlob: PutBlobResult) => {
-        setBlob(newBlob);
-        setMessage("Upload successful");
-      })
-      .catch((error) => {
-        setMessage("Failed to upload: " + error.message);
-      })
-      .finally(() => {
-        setIsLoading(false);
+    try {
+      const response = await fetch(`/api/upload?filename=${file.name}`, {
+        method: "POST",
+        body: file,
       });
+
+      if (!response.ok) {
+        throw new Error("Failed to upload");
+      }
+
+      const newBlob = (await response.json()) as PutBlobResult;
+      setBlob(newBlob);
+      setMessage("Upload successful");
+    } catch (error) {
+      setMessage("Failed to upload: " + (error as Error).message);
+    } finally {
+      setIsLoading(false);
+    }
   };
 
-  // const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-  //   event.preventDefault();
-
-  //   if (!inputFileRef.current?.files) {
-  //     throw new Error("No file selected");
-  //   }
-
-  //   setIsLoading(true); // Disable the button by setting isLoading to true
-  //   const file = inputFileRef.current.files[0];
-
-  //   try {
-  //     const response = await fetch(`/api/upload?filename=${file.name}`, {
-  //       method: "POST",
-  //       body: file,
-  //     });
-
-  //     if (!response.ok) {
-  //       setMessage("Failed to upload");
-  //       throw new Error("Failed to upload");
-  //     }
-
-  //     const newBlob = (await response.json()) as PutBlobResult;
-  //     setBlob(newBlob);
-  //   } catch (error) {
-  //     setMessage("Failed to upload");
-  //   } finally {
-  //     setIsLoading(false); // Re-enable the button by setting isLoading to false
-  //     setMessage("upload successful");
-  //   }
-  // };
   return (
     <>
       <h1>Upload Your File</h1>
