007f46861502f37168a6b8adc8ba442eec0d24c8
OKhorshid <okhorshid.00@gmail.com>
2024-07-30 19:52:41 +0300

    configuring types folder alias

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: 'my-blob-store.public.blob.vercel-storage.com',
-          port: '',
-        },
-      ],
-    },
-  };
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "my-blob-store.public.blob.vercel-storage.com",
+        port: "",
+      },
+    ],
+  },
+  webpack: (config, options) => {
+    config.resolve.alias["@types"] = path.resolve(__dirname, "types");
+    return config;
+  },
+};
    
 export default nextConfig;
