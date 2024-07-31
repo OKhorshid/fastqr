a108589912dd8aeafd7b4a0633b190941e022e18
OKhorshid <okhorshid.00@gmail.com>
2024-07-31 17:10:47 +0300

    hotfix config

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,10 @@
+import path from "path";
+
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
@@ -9,10 +16,10 @@ const nextConfig = {
       },
     ],
   },
-  webpack: (config, options) => {
+  webpack: (config, { options }) => {
     config.resolve.alias["@types"] = path.resolve(__dirname, "types");
     return config;
   },
 };
-   
+
 export default nextConfig;
